dbdb70a6753f87ef58e2da6da6382f67e1c35a30
jun <dongrim318@gmail.com>
2021-12-28 11:49:23 +0900

    version of gatsby-plugin-sharp is changed from 3.14.1 to 4.4.0 and gatsby-node.js file is on

---
+++ gatsby-node.js
@@ -0,0 +1,12 @@
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        path: require.resolve("path-browserify"),
+      },
+      fallback: {
+        fs: false,
+      },
+    },
+  })
+}
