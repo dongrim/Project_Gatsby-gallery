2753ea89d46b7621a51935284248b5952d6b915a
jun <dongrim318@gmail.com>
2021-12-28 12:59:30 +0900

    move window.netlifyIdentity to useEffect from Helmet as a form in script tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Helmet } from "react-helmet"
 import { Layout, Main, Category } from "../components/index"
 
 function IndexPage() {
+  useEffect(() => {
+    const netlifyIdentity = window.netlifyIdentity
+
+    if (!netlifyIdentity) return
+
+    netlifyIdentity.on("init", user => {
+      if (!user)
+        netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/"
+        })
+    })
+  }, [])
+
   return (
     <>
       <Helmet>
         <title>INHO</title>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        <script>
-          if (window.netlifyIdentity){" "}
-          {window.netlifyIdentity.on("init", user => {
-            if (!user) {
-              window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/"
-              })
-            }
-          })}
-        </script>
       </Helmet>
       <Layout>
         <Main />
